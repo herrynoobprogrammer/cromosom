2a59f9b2d2b8a40b17c4b6539e54a0ef8c31babc
herrynoobprogrammer <herinuraliim@gmail.com>
2021-12-19 22:02:49 +0700

    new update

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+for(int o=0; o<=total_mutation-1; o++){
+        //     mutation_kro[o] = (rand()%10);
+        //     mutation_gen[o] = (rand()%4);
+        //     mutation_value[o] = (float)(rand()%30);
+        //     C[(mutation_kro[o])][(mutation_gen[o])] = mutation_value[o];
+        //     printf("\nKromosom[%i]gen[%i] \t\t|| %f\n",mutation_kro[o],mutation_gen[o],mutation_value[o]);
+        // for(int m=0; m<=3; m++)
+        //     {
+        //         printf("%f;",C[(mutation_kro[o])][m]);
+        //     }
+        // printf("\n");
+        // }
